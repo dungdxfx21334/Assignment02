f859c3f7aa1beda51e9c566eb00f52cc402d63b9
dungdx21334 <dungdxfx21334@funix.edu.vn>
2023-04-20 11:29:48 +0700

    Add adding and deleting function

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-'use strict';
+"use strict";
 
+/*  SIDEBAR ANIMATION */
+const sidebar = document.querySelector("nav");
+
+sidebar.addEventListener("click", function () {
+  sidebar.classList.toggle("active");
+});
